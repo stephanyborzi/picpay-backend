752a0105db524860d5955a3d927d8d5b9b200515
stephanyborzi <borzistephany18@outlook.com>
2025-04-27 23:11:53 -0300

    feat: create user repository

---
+++ picpay-backend/src/main/java/com/picpay_backend/picpay_backend/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.picpay_backend.picpay_backend.repositories;
+
+import com.picpay_backend.picpay_backend.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByDocument(String documento);
+    Optional<User> findByDocument(Long id);
+ }
