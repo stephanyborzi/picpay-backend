d99ede895586d918736bd84de469a6c15bfad401
stephanyborzi <borzistephany18@outlook.com>
2025-04-27 23:11:38 -0300

    feat: create transaction entity

---
+++ picpay-backend/src/main/java/com/picpay_backend/picpay_backend/domain/transaction/Transaction.java
@@ -0,0 +1,35 @@
+package com.picpay_backend.picpay_backend.domain.transaction;
+
+import com.picpay_backend.picpay_backend.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transaction")
+@Table(name = "transaction")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+}
