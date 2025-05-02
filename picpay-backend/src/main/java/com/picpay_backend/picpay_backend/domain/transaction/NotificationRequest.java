945d0a181faa21026cb0ecb53eb54f1c2069ea0f
stephanyborzi <borzistephany18@outlook.com>
2025-05-01 23:27:56 -0300

    feat: notification objets to request

---
+++ picpay-backend/src/main/java/com/picpay_backend/picpay_backend/domain/transaction/NotificationRequest.java
@@ -0,0 +1,31 @@
+package com.picpay_backend.picpay_backend.domain.transaction;
+
+public class NotificationRequest {
+    private Long userId;
+    private String email;
+    private String message;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
